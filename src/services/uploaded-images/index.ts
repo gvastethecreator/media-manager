a6d1970720399ca159b20d498823297af0bb7807
GVASTETHECREATOR <920957+gvastethecreator@users.noreply.github.com>
2025-06-12 17:26:47 -0300

    feat: Implementar funciones para asegurar la serialización de datos binarios en imágenes y mejorar la gestión de thumbnails

---
+++ src/services/uploaded-images/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @file Punto de entrada para el servicio de imágenes subidas
+ * @module services/uploaded-images
+ * @description Exporta todas las funcionalidades del servicio de imágenes subidas
+ */
+
+export * from '../uploaded-images.service';
