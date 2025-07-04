b640207124cf399b5e9da899e6c3c5b65e25dbe5
GVASTETHECREATOR <920957+gvastethecreator@users.noreply.github.com>
2025-07-04 15:59:16 -0300

    feat: Eliminar archivos de configuración obsoletos y agregar script de desarrollo con hot reload para el servidor

--- vitest.config.ts
+++ vitest.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
 				'**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}',
 				'**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}',
 				'**/__tests__/**',
-				'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+				'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,build}.config.*',
 				'**/.{eslint,mocha,prettier}rc.{js,cjs,yml}',
 			],
 		},
